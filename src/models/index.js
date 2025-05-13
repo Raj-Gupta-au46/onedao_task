47fddf2063aee997e66562339819eb70e64983e4
Raj Gupta <100614378+Raj-Gupta-au46@users.noreply.github.com>
2025-05-13 16:55:59 +0530

    testing

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,8 @@ const User = require("./userModel");
 const Product = require("./productModel");
 const UserOtp = require("./otpModel");
 
+// correcting the model
+
 module.exports = {
   User,
   Product,
